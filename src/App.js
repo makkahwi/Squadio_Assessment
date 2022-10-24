7d49694b9b939a26daed8c844ec8ebad823b8d5e
makkahwi <suhaibahmadai@hotmail.com>
2022-10-24 07:32:05 +0300

    Build User Inputs Form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import './App.css';
 import logo from './logo.svg';
 
 function App() {
   const [chartData, setChartData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({ interval: "", from: "", to: "" });
 
   const ticker = "SPUS"
-  const from_timestamp = new Date("2022-01-01")
-  const to_timestamp = new Date()
-  const interval = "1mo"
 
-  const getData = async () => {
+  const getChartData = async () => {
     setLoading(true);
 
-    await axios.get(`https://query1.finance.yahoo.com/v7/finance/download/${ticker}?period1=${from_timestamp}&period2=${to_timestamp}&interval=${interval}&events=history`)
+    await axios.get(`https://query1.finance.yahoo.com/v7/finance/download/${ticker}?period1=${formData.from}&period2=${formData.to}&interval=${formData.interval}&events=history`)
       .then(({ data }) => {
         console.log({ data });
         setChartData(data);
@@ -28,25 +26,76 @@ function App() {
       });
   };
 
-  useEffect(() => {
-    getData()
+  useLayoutEffect(() => {
+    getChartData()
   }, []);
+
+  const intervalOptions = [
+    { value: "1d", label: "1 Day" },
+    { value: "1wk", label: "1 Week" },
+    { value: "1mo", label: "1 Month" },
+  ];
+
+  const onChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData(current => ({ ...current, [name]: value }))
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    getChartData();
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <form onSubmit={onSubmit}>
+          <div>
+            <label>
+              {("Interval")}
+            </label>
+
+            <select name="interval" value={formData.interval} onChange={onChange}>
+              {intervalOptions.map(({ value, label }, i) => (
+                <option key={i} value={value} label={label} />
+              ))}
+            </select>
+          </div>
+
+          <div>
+            <label>
+              {("Date Range")}
+            </label>
+
+            <label>
+              {("From")}
+            </label>
+            <input
+              type="date"
+              name="from"
+              value={formData.from}
+              onChange={onChange}
+            />
+
+            <label>
+              {("To")}
+            </label>
+            <input
+              type="date"
+              name="to"
+              value={formData.to}
+              onChange={onChange}
+            />
+          </div>
+
+          <button type="submit" disabled={loading}>
+            {("Get Data")}
+          </button>
+        </form>
       </header>
     </div>
   );
