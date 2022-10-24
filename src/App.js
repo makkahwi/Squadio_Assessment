5c3fef32f40c42f266e759ac8a122c8f214c06f8
makkahwi <suhaibahmadai@hotmail.com>
2022-10-24 07:15:05 +0300

    Build Initial API Call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './App.css';
 import logo from './logo.svg';
 
-import './App.css';
-
 function App() {
+  const [chartData, setChartData] = useState([]);
+
+  const ticker = "SPUS"
+  const from_timestamp = new Date("2022-01-01")
+  const to_timestamp = new Date()
+  const interval = "1mo"
+
+  const getData = async () => {
+    await axios.get(`https://query1.finance.yahoo.com/v7/finance/download/${ticker}?period1=${from_timestamp}&period2=${to_timestamp}&interval=${interval}&events=history`)
+      .then(({ data }) => {
+        console.log({ data });
+        setChartData(data);
+      })
+      .catch(error => {
+        console.log({ error })
+      });
+  };
+
+  useEffect(() => {
+    getData()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
