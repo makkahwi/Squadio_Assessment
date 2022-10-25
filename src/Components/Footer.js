412e871652ee56ffd8bc0b79c6a685a7302136f4
makkahwi <suhaibahmadai@hotmail.com>
2022-10-25 08:52:31 +0300

    Footer URL Fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
       <footer className="bg-dark text-center text-white position-absolute bottom-0 w-100">
         <div className="text-center p-3">
           {new Date().getFullYear()} {("Copyright © ")}
-          <a className="text-white" href="https://suahib.dev" target="_blank" rel="noreferrer">Suhaib Ahmad</a>
+          <a className="text-white" href="https://Suhaib.dev" target="_blank" rel="noreferrer">Suhaib Ahmad</a>
         </div>
       </footer>
     </>
